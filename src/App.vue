1431c934cf0a946f932f8273f503ee30c1d5a3c5
Francisco Prudente Correa <franciscopcorrea@gmail.com>
2024-08-25 18:31:04 -0300

    corrigindo exercícios

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <script setup>
 import { reactive } from 'vue';
-import Componente from './components/Componente.vue'
+
 
 
 //constantes e objetos//
@@ -104,10 +104,40 @@ function operacaoMatematica() {
 
 <template>
 <div class="container">
-  <Componente :operacao-matematica="operacaoMatematica()" :-chama-um-numero="ChamaUmNumero()" 
-  :-chama-dois-numeros="ChamaDoisNumeros()" :-soma="Soma()" :-subtrai="Subtrai()" :-divide="Divide()" 
-  :-raiz="Raiz()" :-potencia="Potencia()" :-fatorial="Fator()" :-multiplica="Multiplica()" :aaa="estado.aaa" :chamamumeros="estado.chamanumeros" 
-  :numero1="estado.numero1", :numero2="estado.numero2" :numero3="estado.numero3" />
+  <h1>
+        Calculadora Inteligente
+    </h1>
+    <h4>Calculadora com dois números</h4>
+    <ul @click="ChamaDoisNumeros" class="dois_numeros">
+        <li @click="Soma">Soma</li>
+        <li @click="Subtrai">Subtração</li>
+        <li @click="Multiplica">Multiplicação</li>
+        <li @click="Divide">Divisão</li>
+    </ul>
+    <h4>Calculadora com um número</h4>
+    <ul @click="ChamaUmNumero" class="um_numero">
+        <li @click="Potencia">Pontência</li>
+        <li @click="Raiz">Raiz Quadrada</li>
+        <li @click="Fator">Fatorial</li>
+    </ul>
+    <div class="container-inputs">
+        <div v-if="estado.chamanumeros" class="container-numero-2">
+            <label for="">Número 1:</label>
+            <input type="number" @keyup="evento => estado.numero1 = parseFloat(evento.target.value)"
+                placeholder="Digite um número">
+            <label for="">Número 2:</label>
+            <input type="number" @keyup="evento => estado.numero2 = parseFloat(evento.target.value)"
+                placeholder="Digite um número">
+        </div>
+        <div v-if="estado.aaa" class="container-numero-1">
+            <label for="">Número:</label>
+            <input type="number" @keyup="evento => estado.numero3 = parseFloat(evento.target.value)"
+                placeholder="Digite um número">
+        </div>
+    </div>
+    <span>
+        {{ operacaoMatematica() }}
+    </span>
 </div>
 
 
